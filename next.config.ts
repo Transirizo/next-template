0129629bcca34ec25fd87e1e506d75f6168c8d42
yiheee-lab <lab.yihe.ee>
2025-07-14 06:54:26 +0800

    chore: add cicd.yaml to control build environment; add --prod for removing eslint; move all files to .aliyun folder for better structure; remove standalone config.

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const nextConfig: NextConfig = {
-    // 阿里云 FC 部署需要设置为 standalone
-    output: 'standalone',
 };
 
 const withNextIntl = createNextIntlPlugin('./src/lib/i18n/request.ts');
